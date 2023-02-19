ff6c4d7a11924d88bdbaea92c17781194816cb3c
AbhishekMarda <abhishek1marda@ucla.edu>
2023-02-19 04:30:48 -0800

    Adds task completion checking function

--- convex-functions/convex/requestSpecifiedAvailableCompute.js
+++ convex-functions/convex/requestSpecifiedAvailableCompute.js
@@ -8,5 +8,6 @@ export default mutation(async ({ db }, host_id, client_id, code, requirements) =
     const response = ""
 
     const request = {client_id, code, complete_flag, host_id, requirements, response};
-    await db.insert("requests", request);
+    const id = await db.insert("requests", request);
+    return await db.query('requests').filter(q => q.eq(q.field('_id'), id)).unique();
 });
